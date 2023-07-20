aa24aecfa31a5916b0bcf905e4b83156125fb3ec
Muhammad Abubakar <muhammad.abubakar@cosociety.co>
2023-07-21 01:00:50 +0500

    Comit Add Component Folder

--- src/Pages/UserReview.tsx
+++ src/Pages/UserReview.tsx
@@ -4,7 +4,6 @@ function UserReview() {
   return (
     <>
     <div className='flex justify-center items-center h-screen bg-black'>
-
         <h1 className='text-white'>User Review Page</h1>
     </div>
     </>
