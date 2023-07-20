b166a291da62c104a168a111ed79ed9904ccf81b
Muhammad Abubakar <muhammad.abubakar@cosociety.co>
2023-07-21 00:08:04 +0500

    initial Comit

---
+++ src/Pages/AgenceyDetails.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function AgenceyDetails() {
+  return (
+    <div className='flex justify-center items-center h-screen bg-black'>
+        <h1 className='text-white'>Agency Page</h1>
+    </div>
+  )
+}
+export default AgenceyDetails
---
+++ src/Pages/UserReview.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function UserReview() {
+  return (
+    <>
+    <div className='flex justify-center items-center h-screen bg-black'>
+
+        <h1 className='text-white'>User Review Page</h1>
+    </div>
+    </>
+  )
+}
+
+export default UserReview
