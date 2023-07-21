2e0bf07d4eaf868a86b300e77bd681845a5771c7
WELCOME\pc <aaqib.javaid.0000@gmail.com>
2023-07-21 11:06:15 +0500

    pagination added

--- src/Pages/AgenceyDetails.tsx
+++ src/Pages/AgenceyDetails.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Sections3 from "../Sections/Home/Sections3";
 import Section2 from "../Sections/Home/Section2";
+import Pagination from "../Sections/Home/Pagination/Pagination";
+import Footer from "../Sections/Home/Footer/Footer";
 function AgenceyDetails() {
   return (
     <>
       <Section2 />
       <Sections3 />
        <Sections3 />
+       <Sections3 />
+       <Sections3 />
+       <Sections3 />
+       <Sections3 />
+       <Sections3 />
+       <Pagination/>
+       <Footer/>
     </>
   );
 }
---
+++ src/Sections/Home/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div className="border-t my-10 max-w-[1216px] border-[#EAECF0] mx-[5%] xl:mx-auto "></div>
+      <div className='flex items-center justify-center'>
+        <p className='text-[12px] font-normal leading-[18px] font-montserrat text-[#565656] mb-10'>Copyright © 2022 Agencyreview. All Rights Reserved.</p>
+      </div>
+    </>
+  )
+}
+
+export default Footer
